ccf615cd4263e3336d82eeaddef24c455298d6bd
zaxkkcuyy-cpu <zaxkkcuyy@gmail.com>
2025-09-25 20:22:45 +0700

    BangunDatar2.java

---
+++ BangunDatar2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package overriding2;
+
+/**
+ *
+ * @author Dell 5300
+ */
+
+public class BangunDatar2 {
+    public double luas, keliling;
+
+    public void hitungLuas() {
+        luas = 0;
+    }
+
+    public void hitungKeliling() {
+        keliling = 0;
+    }
+
+    public void tampilData() {
+        System.out.println("LUAS = " + luas);
+        System.out.println("KELILING = " + keliling);
+    }
+}
